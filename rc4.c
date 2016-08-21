fb1e03a6280e0b5bc606ac3037f2762ecb78824d
Luis Colorado <luiscoloradosistemas@gmail.com>
2016-08-21 13:11:20 +0300

    Retab and eliminate references to <malloc.h>
    Signed-off-by: Luis Colorado <luiscoloradosistemas@gmail.com>

--- rc4.c
+++ rc4.c
@@ -43,56 +43,58 @@ static char RC4_C_RCSId[]="\n$Id: rc4.c,v 1.2 2003/12/11 21:35:13 luis Exp $\n";
 /* functions */
 static void rc4advance(RC4STATUS *s, RC4BYTE *key)
 {
-	RC4BYTE c;
+    RC4BYTE c;
 
-	s->i++;
-	s->i %= RC4MOD;
+    s->i++;
+    s->i %= RC4MOD;
 
-	s->j += s->S[s->i];
-	if(key)
-		s->j += key[s->i];
-	s->j %= RC4MOD;
+    s->j += s->S[s->i];
+    if(key)
+        s->j += key[s->i];
+    s->j %= RC4MOD;
 
-	/* SWAP S[i] <--> S[j] */
-	{ RC4BYTE c;
-		c = s->S[s->i];
-		s->S[s->i] = s->S[s->j];
-		s->S[s->j] = c;
-	} /* SWAP */
+    /* SWAP S[i] <--> S[j] */
+    if (s->i != s->j) {
+        RC4BYTE c;
+
+        c = s->S[s->i];
+        s->S[s->i] = s->S[s->j];
+        s->S[s->j] = c;
+    } /* if */
 
 #if DEBUG
-	fprintbuf(stdout, RC4MOD, s->S,
-		"STATUS: i = 0x%02x; j = 0x%02x; K = 0x%02x",
-		s->i, s->j, s->S[(s->S[s->i] + s->S[s->j])%RC4MOD]);
+    fprintbuf(stdout, RC4MOD, s->S,
+        "STATUS: i = 0x%02x; j = 0x%02x; K = 0x%02x",
+        s->i, s->j, s->S[(s->S[s->i] + s->S[s->j])%RC4MOD]);
 #endif
 
 } /* rc4advance */
 
 void rc4init(RC4STATUS *s, RC4BYTE *key)
 {
-	int i;
+    int i;
 
-	/* initialize fields */
-	s->i = s->j = 0;
-	for (i = 0; i < RC4MOD; i++)
-		s->S[i] = i;
+    /* initialize fields */
+    s->i = s->j = 0;
+    for (i = 0; i < RC4MOD; i++)
+        s->S[i] = i;
 
-	/* scramble with the key */
-	for (i = 0; i < RC4MOD; i++)
-		rc4advance(s, key);
+    /* scramble with the key */
+    for (i = 0; i < RC4MOD; i++)
+        rc4advance(s, key);
 
 } /* rc4init */
 
 RC4BYTE rc4next(RC4STATUS *s)
 {
-	rc4advance(s, NULL);
-	return s->S[(s->S[s->i] + s->S[s->j]) % RC4MOD];
+    rc4advance(s, NULL);
+    return s->S[(s->S[s->i] + s->S[s->j]) % RC4MOD];
 } /* rc4next */
 
 void rc4cipher(RC4STATUS *s, RC4BYTE *b, size_t n)
 {
-	for (;n;b++,n--)
-		*b ^= rc4next(s);
+    for (;n;b++,n--)
+        *b ^= rc4next(s);
 } /* rc4cipher */
 
 /* $Id: rc4.c,v 1.2 2003/12/11 21:35:13 luis Exp $ */
--- rc4_main.c
+++ rc4_main.c
@@ -27,7 +27,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <malloc.h>
 #include <pwd.h>
 #include <errno.h>
 #include <fcntl.h>
